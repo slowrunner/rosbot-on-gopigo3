2da71aa2da12446f3d8e4b1d0f3e4ccd46e1c7f0
slowrunner <slowrunner@users.noreply.github.com>
2021-06-17 15:40:38 -0400

    updates

--- totallife.sh
+++ totallife.sh
@@ -9,16 +9,11 @@ echo "(Cleaning life.log first)"
 /home/pi/rosbot-on-gopigo3/cleanlifelog.py
 echo " "
 fn="/home/pi/rosbot-on-gopigo3/life.log"
-declare -i newBattsAtCycle=0
-# awk -F':' '{sum+=$3}END{print "total life: " sum " hrs";}' $fn
 totalLife=`(awk -F':' '{sum+=$3}END{print sum;}' $fn)`
-echo "Total Life: " $totalLife "hrs since June 10, 2021"
-# echo "Sessions (boot) this year: " `(grep -c "\- boot \-" $fn)`
-bootedThisYr=`(grep "\- boot \-" $fn | sort -u -k1,1 | wc -l)`
-echo "Average Time Between Reboot: " $aveSession "hrs"
-echo "New Batteries At Cycle:" $newBattsAtCycle
+echo "Total Life: " $totalLife "hrs (since June 10, 2021)"
+echo "Sessions (boot): " `(grep -c "\- boot \-" $fn)`
+booted=`(grep -c "\- boot \-" $fn)`
+aveSession=`(echo "scale=1; $totalLife / $booted" | bc)`
+echo "Average Session: " $aveSession "hrs"
 safetyShutdowns=`(grep -c "Safety Shutdown" $fn)`
-echo "Safety Shutdowns this year: " $safetyShutdowns 
-playtime=`(grep playtime $fn | awk -F"after" '{sum+=$2}END{print sum;}' )`
-avePlaytime=`(echo "scale=1; $playtime / ($bootedThisYr)" | bc)`
-echo "Ave Playtime this year: " $avePlaytime
+echo "Safety Shutdowns: " $safetyShutdowns 
