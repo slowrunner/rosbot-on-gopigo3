cbe86b3cae058c79bd34118d800a3c8ad3ea344a
slowrunner <slowrunner@users.noreply.github.com>
2021-06-19 15:45:15 -0400

    accuracy statement

--- plib/battery.py
+++ plib/battery.py
@@ -11,6 +11,7 @@ sys.path.insert(1,"/home/pi/rosbot-on-gopigo3/plib/")
 from noinit_easygopigo3 import EasyGoPiGo3
 
 REV_PROTECT_DIODE = 0.81    # The GoPiGo3 has a reverse polarity protection diode drop of 0.6v to 0.8v (n=2)
+                            # This value results in average readings vs battery voltage error of +/- 0.03
 SAFETY_SHUTDOWN_vBatt = 9.75   # Battery Discharge Protection Circuit allows down to 8.15v (9.75 leaves ~15m reserve)
 SAFETY_SHUTDOWN_vReading = SAFETY_SHUTDOWN_vBatt - REV_PROTECT_DIODE   # 8.5v EasyGoPiGo3.volt() reading
 WARNING_LOW_vBatt = 10.0       # Give (~20 minutes) Advance Warning before safety shutdown
